362cc952fd0c46c4e1e8bac89e5a948f330ab197
albretsen <bjellanda@gmail.com>
2024-04-06 18:34:36 +0200

    Updated snapshot test

--- src/app/pages/SignInOIDCPage/index.tsx
+++ src/app/pages/SignInOIDCPage/index.tsx
@@ -35,7 +35,7 @@ export function SignInOIDCPage() {
           height: '100vh',
         }}
       >
-        {true ? (
+        {isLoading ? (
           <PacmanLoader color="#4A3AFF" />
         ) : error ? (
           <p>Error: {error}</p>
