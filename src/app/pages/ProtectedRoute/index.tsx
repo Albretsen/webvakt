37d0f682e5fc625f9d52dbcf1ee0c7ebd35c41d8
albretsen <bjellanda@gmail.com>
2024-04-07 20:44:10 +0200

    Added access token retrieval

--- src/app/pages/ProtectedRoute/index.tsx
+++ src/app/pages/ProtectedRoute/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { MsalAuthenticationTemplate } from '@azure/msal-react';
+import { MsalAuthenticationTemplate, useMsal } from '@azure/msal-react';
+import {
+  InteractionRequiredAuthError,
+  InteractionStatus,
+} from '@azure/msal-browser';
 import { InteractionType } from '@azure/msal-browser';
 import PacmanLoader from 'react-spinners/PacmanLoader';
+import { useEffect, useState } from 'react';
 
 export const ProtectedRoute = () => {
   return (
@@ -11,10 +16,46 @@ export const ProtectedRoute = () => {
       errorComponent={ErrorComponent}
       loadingComponent={LoadingComponent}
     >
+      <ProtectedComponent></ProtectedComponent>
       <Outlet />
     </MsalAuthenticationTemplate>
   );
 };
+
+function ProtectedComponent() {
+  const { instance, inProgress, accounts } = useMsal();
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    const accessTokenRequest = {
+      scopes: [
+        'https://webvaktcom.onmicrosoft.com/c454c749-11df-4138-a7b8-1047b0241bba/user.read',
+      ],
+      account: accounts[0],
+    };
+    if (!apiData && inProgress === InteractionStatus.None) {
+      instance
+        .acquireTokenSilent(accessTokenRequest)
+        .then(accessTokenResponse => {
+          // Acquire token silent success
+          let accessToken = accessTokenResponse.accessToken;
+          console.log('Access token: ', accessToken.toString());
+          // Call your API with token
+          /*callApi(accessToken).then(response => {
+            setApiData(response);
+          });*/
+        })
+        .catch(error => {
+          if (error instanceof InteractionRequiredAuthError) {
+            instance.acquireTokenRedirect(accessTokenRequest);
+          }
+          console.log(error);
+        });
+    }
+  }, [instance, accounts, inProgress, apiData]);
+
+  return <p>Return your protected content here: {apiData}</p>;
+}
 
 function ErrorComponent({ error }) {
   return (
@@ -29,7 +70,6 @@ function ErrorComponent({ error }) {
       An error occurred: {error.errorMessage}
     </div>
   );
-  //return <div>An error occurred: {error.errorMessage}</div>;
 }
 
 function LoadingComponent() {
