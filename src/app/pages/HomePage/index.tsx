993a2f94a8981f92217ae0d907fc63720818e4eb
albretsen <bjellanda@gmail.com>
2024-04-12 09:07:16 +0200

    Added video demonstration component

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Prices from 'app/components/FrontPageComponents/Prices/Prices';
 import Hero from 'app/components/FrontPageComponents/Hero/Hero';
+import VideoDemonstration from 'app/components/FrontPageComponents/VideoDemonstration/VideoDemonstration';
 
 export function HomePage() {
   return (
@@ -12,6 +13,7 @@ export function HomePage() {
       </Helmet>
       <div className="container">
         <Hero />
+        <VideoDemonstration />
         <Prices />
       </div>
     </>
